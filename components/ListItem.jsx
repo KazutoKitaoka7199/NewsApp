278995fe987e8111920e1e61b61b4d5b0e7a4924
KazutoKitaoka7199 <wrkitaokakazuto@gmail.com>
2022-03-07 22:00:53 +0900

    create newsApp

---
+++ components/ListItem.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+
+export default function ListItem({ imageUrl, title, author }) {
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.leftContainer}>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: imageUrl }}
+        />
+      </View>
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3}>
+          {title}
+        </Text>
+        <Text style={styles.subtext}>{author}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flexDirection: 'column',
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    justifyContent: 'space-around',
+  },
+  subtext: {
+    fontSize: 12,
+    color: 'gray',
+  },
+});
